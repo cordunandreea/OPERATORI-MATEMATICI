12dbd75c952bd27c7e163be8fa11df5ad9e1b542
cordunandreea <70647953+cordunandreea@users.noreply.github.com>
2020-10-05 19:33:25 +0300

    g

---
+++ cazul_g.py
@@ -0,0 +1,19 @@
+import math
+a=2*math.sqrt(5)
+b=math.sqrt(6-a)
+c=2*b
+d=math.sqrt(8+c)
+e=4*d
+f=math.sqrt(13-e)
+g=6*f
+hp=math.sqrt(26+g)
+i=2*math.sqrt(5)
+j=math.sqrt(6+i)
+k=2*j
+l=math.sqrt(8-k)
+m=4*l
+n=math.sqrt(13+m)
+o=6*n
+pp=math.sqrt(26-o)
+rezultat=hp+pp
+print(rezultat)
